9466626e229b05e81cd29dd7c344cc0837adfee8
bankah-junior <anthonybekoebankah@gmail.com>
2026-02-10 20:50:20 +0000

    feat: Add transactional support to service methods for improved data integrity

--- SpringBootBloggingApp/src/main/java/com/amalitech/SpringBootBloggingApp/service/TagService.java
+++ SpringBootBloggingApp/src/main/java/com/amalitech/SpringBootBloggingApp/service/TagService.java
@@ -3,6 +3,7 @@ package com.amalitech.SpringBootBloggingApp.service;
 import com.amalitech.SpringBootBloggingApp.model.dto.request.CreateTagRequest;
 import com.amalitech.SpringBootBloggingApp.model.dto.response.PageResponse;
 import com.amalitech.SpringBootBloggingApp.model.entity.Tag;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,6 +12,9 @@ public interface TagService {
     Tag create(Tag tag);
 
     Tag create(CreateTagRequest request);
+
+    @Transactional(readOnly = true)
+    Tag getById(String tagId);
 
     Tag getByName(String name);
 
